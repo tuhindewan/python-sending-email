8d2a5a8ac958ee0ef6207bc6567a1816b55c35e5
tuhindewan <tuhinsshadow@gmail.com>
2020-04-11 20:01:24 +0600

    Embed image to mail body using MIMEImage

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 import smtplib
 
+# Create the root message and fill in from, to, and subject headers
+str_from = "Saiduzzaman Tuhin"
+str_to = "artisan.python@gmail.com"
 message = MIMEMultipart()
-message['from'] = "Saiduzzaman Tuhin"
-message['to'] = "artisan.python@gmail.com"
+message['from'] = str_from
+message['to'] = str_to
 message['subject'] = "This is a test"
 message.attach(MIMEText("Body"))
 
+# add images
+fp = open('image.jpg', 'rb')
+msgImage = MIMEImage(fp.read(), _subtype='jpg')
+fp.close()
+msgImage.add_header('Content-ID', '<image1>')
+message.attach(msgImage)
+
+# Send the email (this example assumes SMTP authentication is required)
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('artisan.python@gmail.com', 'ash1201033m')
+    smtp.login(str_to, 'ash1201033m')
     smtp.send_message(message)
     print('SENT...')
