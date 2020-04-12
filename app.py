a53b86e27f60aab13121b3092d787b3f761deab5
tuhindewan <tuhinsshadow@gmail.com>
2020-04-12 19:51:09 +0600

    Template file done and sent parameter value to template using Template class

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
+from string import Template
+from pathlib import Path
 import smtplib
+
+
+# Email body by HTML Template
+template = Template(Path('template.html').read_text())
+body = template.substitute(name='John')
 
 # Create the root message and fill in from, to, and subject headers
 str_from = "Saiduzzaman Tuhin"
-str_to = "artisan.python@gmail.com"
+str_to = "tuhinsshadow@gmail.com"
 message = MIMEMultipart()
 message['from'] = str_from
 message['to'] = str_to
 message['subject'] = "This is a test"
-message.attach(MIMEText("Body"))
+message.attach(MIMEText(body, 'html'))
 
 # add images
 fp = open('image.jpg', 'rb')
@@ -23,6 +30,6 @@ message.attach(msgImage)
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login(str_to, 'ash1201033m')
+    smtp.login('artisan.python@gmail.com', 'ash1201033m')
     smtp.send_message(message)
     print('SENT...')
